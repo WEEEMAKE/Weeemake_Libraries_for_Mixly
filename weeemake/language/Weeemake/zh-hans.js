0eaaa9ce2372ffed973ac4dabce842f77ce965f6
Juneral <juneral@weeemake.com>
2019-05-06 17:44:35 +0800

    Weeemake for Mixly

--- weeemake/language/Weeemake/zh-hans.js
+++
@@ -1,9 +0,0 @@
-﻿//-----------------Weeemake---------------------------------
-Blockly.WEEEMAKE_MAIN_CONTROL_BOARD_NAME='主控板类型';
-Blockly.WEEEMAKE_MAIN_CONTROL_BOARD='使用';
-Blockly.WEEEMAKE_MAIN_CONTROL_BOARD_TOOLTIP='在上传你的代码之前，请选择主控类型。';
-
-//WeSingleLED Module
-Blockly.WEEEMAKE_SINGLE_LED_TOOLTIP='设置单色LED灯打开或关闭。';
-Blockly.WEEEMAKE_SINGLE_LED='单色LED灯';
-Blockly.WEEEMAKE_SINGLE_LED_SET='设置';
