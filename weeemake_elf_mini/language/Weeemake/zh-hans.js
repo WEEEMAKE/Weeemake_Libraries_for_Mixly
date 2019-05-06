55aa72816dbcd22fdbd0815d42ea2f77df24e0e7
Juneral <juneral@weeemake.com>
2019-05-06 17:46:11 +0800

    Weeemake for Mixly

---
+++ weeemake_elf_mini/language/Weeemake/zh-hans.js
@@ -0,0 +1,15 @@
+﻿//-----------------Weeemake---------------------------------
+Blockly.WEEEMAKE_MAIN_CONTROL_BOARD_NAME='主控板类型';
+Blockly.WEEEMAKE_MAIN_CONTROL_BOARD='使用';
+Blockly.WEEEMAKE_MAIN_CONTROL_BOARD_TOOLTIP='在上传你的代码之前，请选择主控类型。';
+
+//WeSingleLED Module
+Blockly.WEEEMAKE_SINGLE_LED_TOOLTIP='设置单色LED灯打开或关闭。';
+Blockly.WEEEMAKE_SINGLE_LED='单色LED灯';
+Blockly.WEEEMAKE_SINGLE_LED_SET='设置';
+Blockly.Makeblock_GYRO_UPDATE='陀螺仪状态更新';
+Blockly.Makeblock_RGBLedStrip='设置灯带';
+Blockly.Makeblock_MiniFan='设置小风扇';
+Blockly.Makeblock_MOTOR_FORWARD='顺时针';
+Blockly.Makeblock_MOTOR_REVERSE='逆时针';
+Blockly.Makeblock_MOTOR_STOP='停止';
